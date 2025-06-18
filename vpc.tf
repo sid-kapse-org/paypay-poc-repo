1c294e0e2e4d1f48ff9adf256b128c34196d48ba
Siddharth Kapse <sid.kapse@gmail.com>
2025-06-18 11:33:00 +0800

    eks vpc deploy 3

--- vpc.tf
+++ vpc.tf
@@ -10,9 +10,6 @@ module "vpc" {
   private_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
   public_subnets  = ["10.0.2.0/24", "10.0.3.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
   tags = {
     Terraform = "true"
     Environment = "var.environment"
