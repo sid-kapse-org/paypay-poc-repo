ff65ad726fbce1a83ef9f10305baf9db688e8e90
Siddharth Kapse <sid.kapse@gmail.com>
2025-06-18 10:52:42 +0800

    eks vpc deploy

---
+++ vpc.tf
@@ -0,0 +1,20 @@
+#create vpc with cidr = 172.168.0.0/22 as module
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = "sidd-eks-fargate-vpc"
+  cidr = "10.0.0.0/22"
+
+  azs             = ["${var.region}a", "${var.region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "var.environment"
+  }
+}
