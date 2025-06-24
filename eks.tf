2db1085ce30f9aa224d3d4ff21d6e48a1b96874a
Siddharth Kapse <sid.kapse@gmail.com>
2025-06-24 11:20:00 +0800

    eks deleted

--- eks.tf
+++ eks.tf
@@ -1,35 +1,35 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.31"
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 20.31"
 
-  cluster_name    = var.project_name
-  cluster_version = "1.33"
+#   cluster_name    = var.project_name
+#   cluster_version = "1.33"
 
-  # Optional
-  cluster_endpoint_public_access = true
+#   # Optional
+#   cluster_endpoint_public_access = true
 
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
+#   # Optional: Adds the current caller identity as an administrator via cluster access entry
+#   enable_cluster_creator_admin_permissions = true
 
-  subnet_ids = module.vpc.private_subnets
-  vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+#   vpc_id     = module.vpc.vpc_id
 
-  enable_irsa    = true
-  create_kms_key = true
+#   enable_irsa    = true
+#   create_kms_key = true
 
-  fargate_profiles = {
-    app_profile = {
-      name = "app-profile"
-      selectors = [
-        {
-          namespace = "app"
-        }
-      ]
-    }
-  }
+#   fargate_profiles = {
+#     app_profile = {
+#       name = "app-profile"
+#       selectors = [
+#         {
+#           namespace = "app"
+#         }
+#       ]
+#     }
+#   }
 
-  tags = {
-    Terraform   = "true"
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Terraform   = "true"
+#     Environment = var.environment
+#   }
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "vpc_id"           { value = module.vpc.vpc_id }
 output "public_subnets"   { value = module.vpc.public_subnets }
 output "private_subnets"  { value = module.vpc.private_subnets }
-output "cluster_name"     { value = module.eks.cluster_name }
-output "cluster_endpoint" { value = module.eks.cluster_endpoint }
-output "fargate_profiles" { value = module.eks.fargate_profiles }
+# output "cluster_name"     { value = module.eks.cluster_name }
+# output "cluster_endpoint" { value = module.eks.cluster_endpoint }
+# output "fargate_profiles" { value = module.eks.fargate_profiles }
